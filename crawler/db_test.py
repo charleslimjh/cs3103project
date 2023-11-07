9ad00018f79cab07fe1272ed9009a7403b7baf89
Charles Lim Jun Hao <charleslimjh@u.nus.edu>
2023-11-07 21:20:53 +0800

    update test file

--- crawler/db_test.py
+++ crawler/db_test.py
@@ -1,18 +1,3 @@
-# To use the database:
-# 1. import sqlite3 and db in the crawler
-# 2. run db.init_cursorcon() to initialize the cursor and connection objects
-# 3. import the cursor and con object from db
-# 4. intialize the db using db.init_db()
-
-# Copy the following 5 lines of code to the top of your crawler.py file
-'''
-import sqlite3
-import db
-db.init_cursorcon()
-from db import cursor, con
-db.init_db()
-'''
-
 # 1. initialize the db
 import sqlite3
 import db
